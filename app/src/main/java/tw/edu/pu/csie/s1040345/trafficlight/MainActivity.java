c32e5c7303a7aeb0eb49d8ed6abce7caed5fb66d
Vina <s1040345@gm.pu.edu.tw>
2020-06-05 15:03:21 +0800

    2020上機考版本2(吳旻玲)

--- app/src/main/java/tw/edu/pu/csie/s1040345/trafficlight/MainActivity.java
+++ app/src/main/java/tw/edu/pu/csie/s1040345/trafficlight/MainActivity.java
@@ -1,18 +1,17 @@
 package tw.edu.pu.csie.s1040345.trafficlight;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         //設定全螢幕顯示
         View decorView = getWindow().getDecorView();
         decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
@@ -26,8 +25,21 @@ public class MainActivity extends AppCompatActivity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
         setContentView(R.layout.activity_main);
+
+        EditText myEditText = (EditText) findViewById(R.id.green);
+        if ("".equals(myEditText.getText().toString().trim())) {
+            Toast.makeText(this, "燈號的秒數不能為空白", Toast.LENGTH_LONG).show();
+            return;
+        }
+
     }
 
+    public void StartGame(View v){
+        Intent it = new Intent();
+        it.setClass(this, GameActivity.class);
+        startActivity(it);
+        finish();
+    }
 
     public void EndApp(View v){
         finish();
